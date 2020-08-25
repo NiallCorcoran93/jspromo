a39859a02df4e617a123e45243151173ddad9b41
NiallCorcoran93 <66480878+NiallCorcoran93@users.noreply.github.com>
2020-08-25 16:43:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiper2 = new Swiper2('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
